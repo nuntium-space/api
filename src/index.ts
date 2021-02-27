da298a6d09bee0b9b8d1ea638e2a0bdb471297fe
Alex Sandri <me@alexsandri.com>
2021-02-27 19:05:55 +0100

    Init hapi server

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import Hapi from "@hapi/hapi";
+
+const server = Hapi.server({
+    port: 4000,
+    routes: {
+        cors: true,
+        validate: {
+            options: {
+                abortEarly: false,
+            },
+            failAction: async (request, h, error) =>
+            {
+                throw error;
+            },
+        },
+        response: {
+            emptyStatusCode: 204,
+        },
+    },
+});
+
+const init = async () =>
+{
+    server.start();
+}
+
+init();
