5625538c651544f625b0ea893de8a46a3ad0860d
Alex Sandri <me@alexsandri.com>
2021-02-28 19:24:50 +0100

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,107 @@
+/*
+-------
+DOMAINS
+-------
+*/
+
+create domain "id" as varchar(300) check(value like '___\_%');
+
+/*
+    '320' is the maximum length of an email address as documented here:
+
+    https://tools.ietf.org/html/rfc3696#section-3
+*/
+create domain "email_address" as varchar(320);
+
+create domain "password" as varchar(300) check(length(value) >= 10);
+
+/*
+------
+TABLES
+------
+*/
+
+create table "users"
+(
+    "id" id not null,
+    "first_name" varchar(50) not null,
+    "last_name" varchar(50) not null,
+    "email" email_address not null,
+    "password" password not null,
+
+    primary key ("id"),
+
+    check ("id" like 'usr_%')
+);
+
+create table "organizations"
+(
+    "id" id not null,
+    "name" varchar(50) not null,
+    "user" id not null,
+
+    primary key ("id"),
+
+    foreign key ("user") references "users" on update cascade on delete cascade,
+
+    check ("id" like 'org_%')
+);
+
+create table "publishers"
+(
+    "id" id not null,
+    "name" varchar(50) not null,
+    "organization" id not null,
+
+    primary key ("id"),
+
+    foreign key ("organization") references "organizations" on update cascade on delete cascade,
+
+    check ("id" like 'pub_%')
+);
+
+create table "authors"
+(
+    "id" id not null,
+    "user" id not null,
+    "publisher" id not null,
+
+    primary key ("id"),
+
+    foreign key ("user") references "users" on update cascade on delete cascade,
+    foreign key ("publisher") references "publishers" on update cascade on delete cascade,
+
+    check ("id" like 'aut_%')
+);
+
+create table "articles"
+(
+    "id" id not null,
+    "title" varchar(50) not null,
+    "content" varchar(10000) not null,
+    "created_at" timestamp not null,
+    "updated_at" timestamp not null,
+    "author" id not null,
+
+    primary key ("id"),
+
+    foreign key ("author") references "authors" on update cascade on delete cascade,
+
+    check ("id" like 'art_%'),
+    check ("created_at" <= current_timestamp),
+    check ("updated_at" >= "created_at")
+);
+
+create table "sessions"
+(
+    "id" id not null,
+    "user" id not null,
+    "expires_at" timestamp not null,
+
+    primary key ("id"),
+
+    foreign key ("user") references "users" on update cascade on delete cascade,
+
+    check ("id" like 'ses_%'),
+    check ("expires_at" > current_timestamp)
+);
