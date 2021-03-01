ba3eae86ad7c4f14e31fe1e3d1492658a4ebc3a1
Alex Sandri <me@alexsandri.com>
2021-03-01 15:39:12 +0100

    Remove UUID_SCHEMA

--- src/config/schemas.ts
+++ src/config/schemas.ts
@@ -6,7 +6,5 @@ export const EMAIL_SCHEMA = Joi.string().email();
 
 export const PASSWORD_SCHEMA = Joi.string().min(Config.PASSWORD_MIN_LENGTH);
 
-export const UUID_SCHEMA = Joi.string().uuid({ version: "uuidv4" });
-
 export const DATE_SCHEMA = Joi.extend(JoiDate).date().utc().format("YYYY-MM-DD");
 export const DATETIME_SCHEMA = Joi.extend(JoiDate).date().utc().format("YYYY-MM-DDTHH:mm:ss.SSSZ");
