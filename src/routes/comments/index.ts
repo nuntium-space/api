82a5956dc417997acae84b955b7a4fdea19b56a7
Alex Sandri <me@alexsandri.com>
2021-03-20 10:06:43 +0100

    Move comments routes in a separate file

--- src/routes/comments/index.ts
+++ src/routes/comments/index.ts
@@ -1,6 +1,152 @@
 import Boom from "@hapi/boom";
 import { ServerRoute } from "@hapi/hapi";
+import Joi from "joi";
+import { Config } from "../../config/Config";
+import { COMMENT_CREATE_SCHEMA, COMMENT_SCHEMA, COMMENT_UPDATE_SCHEMA, EXPAND_QUERY_SCHEMA, ID_SCHEMA } from "../../config/schemas";
+import { Article } from "../../models/Article";
+import { Comment } from "../../models/Comment";
+import { User } from "../../models/User";
 
 export default <ServerRoute[]>[
-    
+    {
+        method: "GET",
+        path: "/comments/{id}",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: ID_SCHEMA(Config.ID_PREFIXES.COMMENT).required(),
+                }),
+                query: Joi.object({
+                    expand: EXPAND_QUERY_SCHEMA,
+                }),
+            },
+            response: {
+                schema: COMMENT_SCHEMA,
+            },
+        },
+        handler: async (request, h) =>
+        {
+            const comment = await Comment.retrieve(request.params.id, request.query.expand);
+
+            return comment.serialize();
+        },
+    },
+    {
+        method: "GET",
+        path: "/articles/{id}/comments",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: ID_SCHEMA(Config.ID_PREFIXES.ARTICLE).required(),
+                }),
+                query: Joi.object({
+                    parent: ID_SCHEMA(Config.ID_PREFIXES.COMMENT),
+                    expand: EXPAND_QUERY_SCHEMA,
+                }),
+            },
+            response: {
+                schema: Joi.array().items(COMMENT_SCHEMA).required(),
+            },
+        },
+        handler: async (request, h) =>
+        {
+            const article = await Article.retrieve(request.params.id);
+
+            const comments = await Comment.forArticle(article, {
+                parent: request.query.parent ?? null,
+                expand: request.query.expand,
+            });
+
+            return comments.map(comment => comment.serialize());
+        },
+    },
+    {
+        method: "POST",
+        path: "/articles/{id}/comments",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: ID_SCHEMA(Config.ID_PREFIXES.ARTICLE).required(),
+                }),
+                query: Joi.object({
+                    expand: EXPAND_QUERY_SCHEMA,
+                }),
+                payload: COMMENT_CREATE_SCHEMA,
+            },
+            response: {
+                schema: COMMENT_SCHEMA,
+            },
+        },
+        handler: async (request, h) =>
+        {
+            const authenticatedUser = request.auth.credentials.user as User;
+
+            const comment = await Comment.create(
+                {
+                    ...request.payload as any,
+                    article: request.params.id,
+                    user: authenticatedUser.id,
+                },
+                request.query.expand,
+            );
+
+            return comment.serialize();
+        },
+    },
+    {
+        method: "PATCH",
+        path: "/comments/{id}",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: ID_SCHEMA(Config.ID_PREFIXES.COMMENT).required(),
+                }),
+                payload: COMMENT_UPDATE_SCHEMA,
+            },
+            response: {
+                schema: COMMENT_SCHEMA,
+            },
+        },
+        handler: async (request, h) =>
+        {
+            const comment = await Comment.retrieve(request.params.id);
+
+            const authenticatedUser = request.auth.credentials.user as User;
+
+            if (comment.user.id !== authenticatedUser.id)
+            {
+                throw Boom.forbidden();
+            }
+
+            await comment.update(request.payload as any);
+
+            return comment.serialize();
+        },
+    },
+    {
+        method: "DELETE",
+        path: "/comments/{id}",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: ID_SCHEMA(Config.ID_PREFIXES.ARTICLE).required(),
+                }),
+            },
+        },
+        handler: async (request, h) =>
+        {
+            const comment = await Comment.retrieve(request.params.id);
+
+            const authenticatedUser = request.auth.credentials.user as User;
+
+            if (comment.user.id !== authenticatedUser.id)
+            {
+                throw Boom.forbidden();
+            }
+
+            await comment.delete();
+
+            return h.response();
+        },
+    },
 ];
