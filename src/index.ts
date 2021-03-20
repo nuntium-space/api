ed29b0c7023879f9111819969c6747074412f687
Alex Sandri <me@alexsandri.com>
2021-03-20 10:34:16 +0100

    Move subscriptions routes in a separate file

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,13 @@ import {
     ARTICLE_SCHEMA,
     EXPAND_QUERY_SCHEMA,
     ID_SCHEMA,
-    SESSION_CREATE_SCHEMA,
-    SESSION_SCHEMA,
     STRING_SCHEMA,
-    SUBSCRIPTION_CREATE_SCHEMA,
-    SUBSCRIPTION_SCHEMA,
     URL_SCHEMA,
     USER_CREATE_SCHEMA,
     USER_SCHEMA,
     USER_UPDATE_SCHEMA
 } from "./config/schemas";
 import { Article } from "./models/Article";
-import { Bundle } from "./models/Bundle";
 import { Organization } from "./models/Organization";
 import { Session } from "./models/Session";
 import { User } from "./models/User";
@@ -246,37 +241,6 @@ const init = async () =>
     });
 
     server.route({
-        method: "GET",
-        path: "/users/{id}/subscriptions",
-        options: {
-            validate: {
-                params: Joi.object({
-                    id: ID_SCHEMA(Config.ID_PREFIXES.USER).required(),
-                }),
-                query: Joi.object({
-                    expand: EXPAND_QUERY_SCHEMA,
-                }),
-            },
-            response: {
-                schema: Joi.array().items(SUBSCRIPTION_SCHEMA).required(),
-            },
-        },
-        handler: async (request, h) =>
-        {
-            const authenticatedUser = request.auth.credentials.user as User;
-
-            if (request.params.id !== authenticatedUser.id)
-            {
-                throw Boom.forbidden();
-            }
-
-            const subscriptions = await Subscription.forUser(authenticatedUser, request.query.expand);
-
-            return subscriptions.map(subscription => subscription.serialize());
-        }
-    });
-
-    server.route({
         method: "POST",
         path: "/users",
         options: {
@@ -293,78 +257,6 @@ const init = async () =>
             const user = await User.create(request.payload as any);
 
             return user.serialize();
-        }
-    });
-
-    server.route({
-        method: "POST",
-        path: "/users/{id}/subscriptions",
-        options: {
-            validate: {
-                params: Joi.object({
-                    id: ID_SCHEMA(Config.ID_PREFIXES.USER).required(),
-                }),
-                query: Joi.object({
-                    expand: EXPAND_QUERY_SCHEMA,
-                }),
-                payload: SUBSCRIPTION_CREATE_SCHEMA,
-            },
-        },
-        handler: async (request, h) =>
-        {
-            const authenticatedUser = request.auth.credentials.user as User;
-
-            if (request.params.id !== authenticatedUser.id)
-            {
-                throw Boom.forbidden();
-            }
-
-            if (!authenticatedUser.stripe_customer_id)
-            {
-                throw Boom.badImplementation();
-            }
-
-            const bundle = await Bundle.retrieve((request.payload as any).bundle, [ "organization" ]);
-
-            if (!bundle.stripe_price_id || !(bundle.organization instanceof Organization))
-            {
-                throw Boom.badImplementation();
-            }
-
-            if (!bundle.organization.stripe_account_enabled)
-            {
-                throw Boom.badRequest(`The organization that owns the bundle '${bundle.id}' hasn't enabled payments`);
-            }
-
-            if (await authenticatedUser.isSubscribedToBundle(bundle))
-            {
-                throw Boom.conflict(`The user '${authenticatedUser.id}' is already subscribed to the bundle '${bundle.id}'`);
-            }
-
-            await Config.STRIPE.subscriptions
-                .create({
-                    customer: authenticatedUser.stripe_customer_id,
-                    items: [
-                        {
-                            price: bundle.stripe_price_id,
-                            quantity: 1,
-                        },
-                    ],
-                    application_fee_percent: Config.STRIPE_CONNECT_FEE_PERCENT,
-                    transfer_data: {
-                        destination: bundle.organization.stripe_account_id,
-                    },
-                    metadata: {
-                        user_id: authenticatedUser.id,
-                        bundle_id: bundle.id,
-                    },
-                })
-                .catch(async () =>
-                {
-                    throw Boom.badRequest();
-                });
-
-            return h.response();
         }
     });
 
