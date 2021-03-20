8e2be06114a5ad6099c0e84bdcc8decb7108b925
Alex Sandri <me@alexsandri.com>
2021-03-20 09:50:36 +0100

    Add all necessary imports in routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,23 @@
 import articles from "./articles";
+import authors from "./authors";
+import bundles from "./bundles";
+import comments from "./comments";
+import organizations from "./organizations";
+import paymentMethods from "./payment-methods";
+import publishers from "./publishers";
+import sessions from "./sessions";
+import subscriptions from "./subscriptions";
+import users from "./users";
 
-export = [ articles ].flat();
+export = [
+    articles,
+    authors,
+    bundles,
+    comments,
+    organizations,
+    paymentMethods,
+    publishers,
+    sessions,
+    subscriptions,
+    users,
+].flat();
