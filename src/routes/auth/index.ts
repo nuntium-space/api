0d532a701919fb0170bdde1b78916835e2b400e4
Alex Sandri <me@alexsandri.com>
2021-03-30 10:42:42 +0200

    Update auth route paths

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,9 +3,61 @@ import { ServerRoute } from "@hapi/hapi";
 import { Config } from "../../config/Config";
 
 export default <ServerRoute[]>[
+    /*
     {
         method: [ "GET", "POST" ],
-        path: "/auth/callback/twitter",
+        path: "/auth/facebook",
+        options: {
+            auth: {
+              mode: "try",
+              strategy: "facebook",
+            },
+        },
+        handler: (request, h) =>
+        {
+            if (!request.auth.isAuthenticated)
+            {
+                throw Boom.unauthorized();
+            }
+
+            // TODO:
+            // CREDENTIALS: request.auth.credentials
+            // Add user if it does not exist
+            // Create session
+            // Redirect with session id
+
+            return h.redirect(`${Config.CLIENT_HOST}?session_id=${"TODO"}`);
+        },
+    },
+    {
+        method: [ "GET", "POST" ],
+        path: "/auth/google",
+        options: {
+            auth: {
+              mode: "try",
+              strategy: "google",
+            },
+        },
+        handler: (request, h) =>
+        {
+            if (!request.auth.isAuthenticated)
+            {
+                throw Boom.unauthorized();
+            }
+
+            // TODO:
+            // CREDENTIALS: request.auth.credentials
+            // Add user if it does not exist
+            // Create session
+            // Redirect with session id
+
+            return h.redirect(`${Config.CLIENT_HOST}?session_id=${"TODO"}`);
+        },
+    },
+    */
+    {
+        method: [ "GET", "POST" ],
+        path: "/auth/twitter",
         options: {
             auth: {
               mode: "try",
