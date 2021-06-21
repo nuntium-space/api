7f38d06c34514e4ada8b46012fb638c5aab542e7
Alex Sandri <me@alexsandri.com>
2021-06-21 15:50:32 +0200

    Add routes to handle author invites in separate file

--- src/routes/authors/index.ts
+++ src/routes/authors/index.ts
@@ -3,11 +3,9 @@ import { ServerRoute } from "@hapi/hapi";
 import Joi from "joi";
 import { Schema } from "../../config/Schema";
 import { Author } from "../../models/Author";
-import { AuthorInvite } from "../../models/AuthorInvite";
 import { Publisher } from "../../models/Publisher";
 import { Session } from "../../models/Session";
 import { AUTHOR_SCHEMA } from "../../types/author";
-import { AUTHOR_INVITE_SCHEMA } from "../../types/author-invite";
 
 export default <ServerRoute[]>[
     {
@@ -99,62 +97,6 @@ export default <ServerRoute[]>[
                 : await Author.forUser(authenticatedUser, request.query.expand);
 
             return authors.map(author => author.serialize({ for: authenticatedUser }));
-        },
-    },
-    {
-        method: "POST",
-        path: "/authors/invites/{id}/accept",
-        options: {
-            validate: {
-                params: Joi.object({
-                    id: Schema.ID.AUTHOR_INVITE.required(),
-                }),
-            },
-        },
-        handler: async (request, h) =>
-        {
-            const authenticatedUser = (request.auth.credentials.session as Session).user;
-
-            const invite = await AuthorInvite.retrieve(request.params.id);
-
-            if (authenticatedUser.id !== invite.user.id)
-            {
-                throw Boom.forbidden();
-            }
-
-            await invite.accept();
-
-            return h.response();
-        },
-    },
-    {
-        method: "POST",
-        path: "/publishers/{id}/authors/invites",
-        options: {
-            validate: {
-                params: Joi.object({
-                    id: Schema.ID.PUBLISHER.required(),
-                }),
-                payload: AUTHOR_INVITE_SCHEMA.CREATE,
-            },
-        },
-        handler: async (request, h) =>
-        {
-            const publisher = await Publisher.retrieve(request.params.id);
-
-            const authenticatedUser = (request.auth.credentials.session as Session).user;
-
-            if (!publisher.isOwnedByUser(authenticatedUser))
-            {
-                throw Boom.forbidden();
-            }
-
-            await AuthorInvite.create({
-                email: (request.payload as any).email,
-                publisher: publisher.id,
-            });
-
-            return h.response();
         },
     },
     {
