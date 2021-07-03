e4f4537444b4a2003ac3f633961558461a5fb15d
Alex Sandri <me@alexsandri.com>
2021-07-03 14:01:38 +0200

    Add user account linking routes

--- src/routes/accounts/index.ts
+++ src/routes/accounts/index.ts
@@ -2,6 +2,38 @@ import Boom from "@hapi/boom";
 import { ServerRoute } from "@hapi/hapi";
 import Joi from "joi";
 import { Schema } from "../../config/Schema";
+import Utilities from "../../utilities/Utilities";
+
+const accountLinkingRoutes: ServerRoute[] = [ "facebook", "google", "twitter" ].map(provider =>
+{
+    return {
+        method: "GET",
+        path: "/users/{id}/accounts/link/facebook",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: Schema.ID.USER.required(),
+                }),
+            },
+        },
+        handler: async (request, h) =>
+        {
+            const [ authenticatedUser ] = Utilities.getAuthenticatedUser(request);
+
+            if (authenticatedUser.id !== request.params.id)
+            {
+                throw Boom.forbidden();
+            }
+
+            // TODO:
+            // Generate HMAC to link user account (user id is not enough because it is not private)
+
+            const hmac = "TODO";
+
+            return h.redirect(`auth/${request.auth.strategy}?link=${hmac}`);
+        },
+    };
+});
 
 export default <ServerRoute[]>[
     {
@@ -19,4 +51,5 @@ export default <ServerRoute[]>[
             throw Boom.notImplemented();
         },
     },
+    ...accountLinkingRoutes,
 ];
